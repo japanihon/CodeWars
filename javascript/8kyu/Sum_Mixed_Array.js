68f369ca48ff42195be6fdc8cc88ccbdd34603cd
sarah <sarahkharraz@gmail.com>
2020-03-29 12:49:24 +0100

    8kyu - Sum mixed array solution

--- javascript/8kyu/Sum_Mixed_Array.js
+++ javascript/8kyu/Sum_Mixed_Array.js
@@ -1,2 +1,10 @@
 // Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 // Return your answer as a number.
+
+// SOLUTION
+
+function sumMix(x){
+  var convertString = x.map(Number)
+  var reduceToSum = convertString.reduce((a, b) => a + b, 0)
+return reduceToSum
+}

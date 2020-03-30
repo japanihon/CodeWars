6196c2e45dfaed6d81bc9dfbc28a36ee340fa6c5
sarah <sarahkharraz@gmail.com>
2020-03-30 17:55:00 +0100

    Count the Monkeys! solution

--- javascript/8kyu/Count_the_Monkeys.js
+++ javascript/8kyu/Count_the_Monkeys.js
@@ -6,3 +6,13 @@
 //
 // monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // monkeyCount(1) // --> [1]
+
+// SOLUTION
+
+function monkeyCount(n) {
+ let emptyArray = []
+ for (let i = 1; i <= n; i++){
+   emptyArray.push(i);
+    }
+    return emptyArray
+  }

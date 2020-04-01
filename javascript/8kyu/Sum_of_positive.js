0db37655f959f1919883a84d14bd0101eea8e166
sarah <sarahkharraz@gmail.com>
2020-04-01 20:08:00 +0100

    Sum of positive - solution

--- javascript/8kyu/Sum_of_positive.js
+++ javascript/8kyu/Sum_of_positive.js
@@ -3,3 +3,9 @@
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 //
 // Note: if there is nothing to sum, the sum is default to 0.
+
+// SOLUTION
+
+function positiveSum(arr) {
+  return arr.reduce((a, b) => (Math.sign(b) === 1 ? a + b : a), 0);
+}

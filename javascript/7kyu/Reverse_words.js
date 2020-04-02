ae3088281499b1576656a1188715217b4cb5a25c
sarah <sarahkharraz@gmail.com>
2020-04-02 17:13:31 +0100

    JS - Reverse word - solution

--- javascript/7kyu/Reverse_words.js
+++ javascript/7kyu/Reverse_words.js
@@ -3,3 +3,13 @@
 // Examples
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
+
+// SOLUTION
+
+function reverseWords(str) {
+  const spl = str.split(" ");
+  const map = spl.map(x => x.split(""));
+  const mapping = map.map(x => x.reverse().join(""));
+  const join = mapping.join(" ");
+  return join;
+}

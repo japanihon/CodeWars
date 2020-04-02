d9441cefcffc115fdbab5ef7c0ada1121002bd09
sarah <sarahkharraz@gmail.com>
2020-04-02 17:15:34 +0100

    JS - Create phone number - solution

--- javascript/6kyu/Create_Phone_Number.js
+++ javascript/6kyu/Create_Phone_Number.js
@@ -4,3 +4,15 @@
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 // The returned format must be correct in order to complete this challenge.
 // Don't forget the space after the closing parentheses!
+
+// SOLUTION
+
+function createPhoneNumber(numbers) {
+  let join = numbers.join("");
+  let first = join.substring(0, 3);
+  let second = join.substring(3, 6);
+  let third = join.substring(6, 10);
+  console.log(first, second, third);
+  let phone = `(${first})` + ` ${second}` + `-${third}`;
+  return phone;
+}

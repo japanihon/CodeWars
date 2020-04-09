c70e002255a81c047bb671daf1f5732760b97998
sarah <sarahkharraz@gmail.com>
2020-04-09 12:51:29 +0100

    JS - Add length - solution

--- javascript/8kyu/Add_Length.js
+++ javascript/8kyu/Add_Length.js
@@ -5,3 +5,11 @@
 // Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 //
 // Note: String will have at least one element; words will always be separated by a space.
+
+// SOLUTION
+
+function addLength(str) {
+  let spl = str.split(" ");
+  let map = spl.map(word => `${word} ${word.length}`);
+  return map;
+}

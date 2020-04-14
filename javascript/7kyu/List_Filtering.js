38ec51361ec43cbe1c0f35678ed29c7986202820
sarah <sarahkharraz@gmail.com>
2020-04-14 14:42:31 +0100

    JS - List filtering - solution

--- javascript/7kyu/List_Filtering.js
+++ javascript/7kyu/List_Filtering.js
@@ -4,3 +4,10 @@
 // filter_list([1,2,'a','b']) == [1,2]
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+// SOLUTION
+
+function filter_list(l) {
+  let filt = l.filter(num => Number.isInteger(num));
+  return filt;
+}

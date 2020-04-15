b1db97f6252def23a58fb15a21e1075bd8898ed6
sarah <sarahkharraz@gmail.com>
2020-04-15 15:29:40 +0100

    JS - Highest and lowest - solution

--- javascript/7kyu/Highest_and_Lowest.js
+++ javascript/7kyu/Highest_and_Lowest.js
@@ -10,3 +10,14 @@
 // All numbers are valid Int32, no need to validate them.
 // There will always be at least one number in the input string.
 // Output string must be two numbers separated by a single space, and highest number is first.
+
+// SOLUTION
+
+function highAndLow(numbers) {
+  let spl = numbers.split(" ");
+  let map = spl.map(x => Number(x));
+  let max = String(Math.max(...map));
+  let min = String(Math.min(...map));
+  let concat = max.concat(" ", `${min}`);
+  return concat;
+}

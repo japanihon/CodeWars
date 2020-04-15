c0e6ca2f4195e6da977fe8c04233917dab328ebc
sarah <sarahkharraz@gmail.com>
2020-04-15 15:28:03 +0100

    JS - Mumbling - solution

--- javascript/7kyu/Mumbling.js
+++ javascript/7kyu/Mumbling.js
@@ -6,3 +6,15 @@
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+// SOLUTION
+
+function accum(s) {
+  let spl = s.split("");
+  let mapping = spl.map((x, index) => {
+    let repeat = x.repeat(index + 1);
+    return repeat.charAt(0).toUpperCase() + repeat.slice(1).toLowerCase();
+    //      console.log(repeat.charAt(0).toUpperCase() + repeat.slice(1).toLowerCase())
+  });
+  return mapping.join("-");
+}

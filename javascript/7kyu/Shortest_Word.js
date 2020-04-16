eb2b4eb27aa8db762554a2411301de84768b6c40
sarah <sarahkharraz@gmail.com>
2020-04-16 14:37:10 +0100

    JS - Shortest word - solution

--- javascript/7kyu/Shortest_Word.js
+++ javascript/7kyu/Shortest_Word.js
@@ -1,3 +1,12 @@
 // Simple, given a string of words, return the length of the shortest word(s).
 //
 // String will never be empty and you do not need to account for different data types.
+
+// SOLUTION
+
+function findShort(s) {
+  const spl = s.split(" ");
+  const shorter = (left, right) => (left.length <= right.length ? left : right);
+  const num = spl.reduce(shorter);
+  return num.length;
+}

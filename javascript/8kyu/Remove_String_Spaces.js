a431266216b5de657064f6f95f545889b0558a20
sarah <sarahkharraz@gmail.com>
2020-04-22 20:46:52 +0100

    JS - Remove String Spaces - solution

--- javascript/8kyu/Remove_String_Spaces.js
+++ javascript/8kyu/Remove_String_Spaces.js
@@ -1,1 +1,17 @@
 // Simple, remove the spaces from the string, then return the resultant string.
+
+// SOLUTION
+
+function noSpace(phrase) {
+  let string = "";
+
+  for (let index = 0; index < phrase.length; index++) {
+    let character = phrase.charAt(index);
+
+    if (character !== " ") {
+      string = string + character;
+    }
+  }
+
+  return string;
+}

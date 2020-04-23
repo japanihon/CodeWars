0dbc76255ca2be280272f4f905fa495c6c3fe286
sarah <sarahkharraz@gmail.com>
2020-04-23 11:01:33 +0100

    JS - Return negative - solution

--- javascript/8kyu/Return_Negative.js
+++ javascript/8kyu/Return_Negative.js
@@ -10,3 +10,9 @@
 //
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+// SOLUTION
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}

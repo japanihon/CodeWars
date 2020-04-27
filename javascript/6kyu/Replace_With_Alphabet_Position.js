708e737c8e8ed0cb2a7147434272c56e683a8987
sarah <sarahkharraz@gmail.com>
2020-04-27 11:09:07 +0100

    JS - Replace With Alphabet Position - solution

--- javascript/6kyu/Replace_With_Alphabet_Position.js
+++ javascript/6kyu/Replace_With_Alphabet_Position.js
@@ -9,3 +9,51 @@
 // Example
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+// SOLUTION
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+function splitAlphabet(text) {
+  return text.split("");
+}
+
+function convertLetterToNumber(array) {
+  const firstConvert = array.map(
+    letter => alphabet.indexOf(letter.toLowerCase()) + 1
+  );
+  return firstConvert.filter(number => number >= 1 && number <= 27);
+}
+
+function alphabetPosition(text) {
+  const arrayOfLetter = splitAlphabet(text);
+  const arrayOfNumbers = convertLetterToNumber(arrayOfLetter);
+  return arrayOfNumbers.join(" ");
+}

482004f5c5cba7470319ca375e5cfdaaebcdb775
sarah <sarahkharraz@gmail.com>
2020-04-29 17:11:26 +0100

    RB - Even or odd - solution

--- ruby/8kyu/Even_or_Odd.rb
+++ ruby/8kyu/Even_or_Odd.rb
@@ -1,1 +1,11 @@
 # Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+# SOLUTION
+
+def even_or_odd(number)
+  if number %2 == 0
+   'Even'
+  else
+   'Odd'
+  end
+end

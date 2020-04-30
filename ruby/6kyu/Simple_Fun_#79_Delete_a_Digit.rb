58b56f4b8cad2f3a60e2f0443e23ef99adfadb3e
sarah <sarahkharraz@gmail.com>
2020-04-30 13:43:29 +0100

    RB - Simple fun 79 delete a digit - solution

--- ruby/6kyu/Simple_Fun_#79_Delete_a_Digit.rb
+++ ruby/6kyu/Simple_Fun_#79_Delete_a_Digit.rb
@@ -12,3 +12,19 @@
 # Constraints: 10 ≤ n ≤ 1000000.
 #
 # [output] an integer
+
+# SOLUTION
+
+def delete_digit(n)
+#     split_number = n.to_s.split("")
+#     smaller_digit = split_number.min
+#     smaller_digit.clear
+#     p split_number
+#     (split_number.join).to_i
+# end
+ (0...n.to_s.size).map { |i|
+   s = n.to_s.chars
+   s.delete_at(i)
+   s.join.to_i
+ } .max
+end

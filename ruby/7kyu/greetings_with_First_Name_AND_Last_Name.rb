5b78625a7c145de296a75402eb1d2d2774e48817
sarah <sarahkharraz@gmail.com>
2020-05-06 22:12:41 +0100

    RB - greetings with First Name AND Last Name - ex

---
+++ ruby/7kyu/greetings_with_First_Name_AND_Last_Name.rb
@@ -0,0 +1,1 @@
+# Create a Person class with a greet method that returns the first and last name of the person with a greeting. For example if the first name is 'Bob' and the last name is 'Smith' then it should return 'Hello, Bob Smith!'.

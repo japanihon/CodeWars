f9152271cf0762db58d8cfb167264d13104b51a9
sarah <sarahkharraz@gmail.com>
2020-05-08 23:03:18 +0100

    RB - Enumerable Magic #28 - The Least and the Greatest, part 2 - ex

---
+++ ruby/8kyu/Enumerable_Magic_#28_The_Least_and_the_Greatest_part_2.rb
@@ -0,0 +1,11 @@
+# Create a method minmax_by that accepts a list and a block. The method should return an array containing the min and max list elements, based on their return values from the block
+#
+# Here's a simple example:
+#
+# minmax([3,2,5,4]){|item| item}  #=> [2,5]
+# minmax([2,14,7,9]){|item| item.to_s}  #=> [14,9]
+# If the list is empty, you should return [nil, nil].
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-minmax-by

7f9c1ec89f39f46ddf465fe0915ac3543fc187aa
sarah <sarahkharraz@gmail.com>
2020-05-08 23:05:06 +0100

    RB - Enumerable Magic #32 - Zip Up! - ex

---
+++ ruby/8kyu/Enumerable_Magic_#32_Zip_Up.rb
@@ -0,0 +1,9 @@
+# Create a method zip that accepts two lists of the same length, and combines their result into a single array, like so:
+#
+# first = ['a', 'c', 'e']
+# second = ['b', 'd', 'f']
+# zip(first, second)
+#     #=> ['a', 'b', 'c', 'd', 'e', 'f']
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-zip/

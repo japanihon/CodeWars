0db62b6f5c3c90bdd3541e0e5fa8f924e965d6b6
sarah <sarahkharraz@gmail.com>
2020-05-13 14:56:35 +0100

    RB - Enumerable Magic #21 - Filter that Array! - ex

---
+++ ruby/8kyu/Enumerable_Magic_#21_Filter_that_Array.rb
@@ -0,0 +1,5 @@
+# Create a method select that accepts a list and a block, and returns a new array of elements for which the given block returns true.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-select

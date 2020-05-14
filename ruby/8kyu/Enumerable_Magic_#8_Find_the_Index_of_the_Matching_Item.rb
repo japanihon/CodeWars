40571263c8de575cfdef96efbfa88ff864664536
sarah <sarahkharraz@gmail.com>
2020-05-14 23:37:30 +0100

    RB - Enumerable Magic #8 - Find the Index of the Matching Item - ex

---
+++ ruby/8kyu/Enumerable_Magic_#8_Find_the_Index_of_the_Matching_Item.rb
@@ -0,0 +1,5 @@
+# Create a method find_index that accepts a list and a block, and returns index of the first item for which the block returns true. If no item in the list matches, return nil.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-find-index

e71e88ee9828913979f0d971710021da5ceea1f1
sarah <sarahkharraz@gmail.com>
2020-05-14 23:39:29 +0100

    RB - Enumerable Magic #23 - Filter Array by Regex - ex

---
+++ ruby/8kyu/Enumerable_Magic_#23_Filter_Array_by_Regex.rb
@@ -0,0 +1,5 @@
+# Create a method grep that accepts a list and a regexp pattern, and returns a list of elements which match the pattern.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-grep

1f9ae212a02f71d53c6b6de9337bc0e5c9d76d02
sarah <sarahkharraz@gmail.com>
2020-05-15 19:24:40 +0100

    RB - Enumerable Magic #14 - Map that Array! - ex

---
+++ ruby/8kyu/Enumerable_Magic_#14_Map_that_Array.rb
@@ -0,0 +1,5 @@
+# Create a method map that accepts a list and a block, runs the block for each item in the list, and returns a new array with the block return values.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-map

a1e763670d7357b28790b2cf8d4688bb9109d371
sarah <sarahkharraz@gmail.com>
2020-05-15 19:21:20 +0100

    RB - Enumerable Magic #19 - Skip Array Elements While... - ex

---
+++ ruby/8kyu/Enumerable_Magic_#19_Skip_Array_Elements_While.rb
@@ -0,0 +1,5 @@
+# Create a method drop_while that accepts a list and a block, and returns a copy of the list that skips over elements from the left, for as long as the given block returns true.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-drop-while

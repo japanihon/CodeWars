5116ca872309baa7ac551c134b383623fdaa3bc2
sarah <sarahkharraz@gmail.com>
2020-05-18 14:21:57 +0100

    RB - Enumerable Magic #12 - Find Minimum Value by Comparison - solution

--- ruby/8kyu/Enumerable_Magic_#12_Find_Minimum_Value_by_Comparison.rb
+++ ruby/8kyu/Enumerable_Magic_#12_Find_Minimum_Value_by_Comparison.rb
@@ -1,7 +1,5 @@
 # Create a method min that accepts a list and a block, runs the block for each comparison in the list, and returns the item with the lowest value.
-#
 # If you need help, here's a reference:
-#
 # http://www.rubycuts.com/enum-min
 
 # SOLUTION

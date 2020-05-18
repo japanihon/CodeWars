fd34b09be43be245753592d0808501a671629274
sarah <sarahkharraz@gmail.com>
2020-05-18 14:27:48 +0100

    RB - Enumerable Magic #27 - The Least and the Greatest - ex

---
+++ ruby/8kyu/Enumerable_Magic_#27_The_Least_and_the_Greatest.rb
@@ -0,0 +1,9 @@
+# Create a method minmax that accepts a list and a block. The block itself should compare two elements, so the method can sort the elements and return the minimum and maximum as a 2-element array.
+#
+# Here's a simple example:
+#
+# minmax([3,2,5,4]){|a,b| a <=> b}  #=> [2,5]
+# minmax([2,14,7,9]){|a,b| a.to_s <=> b.to_s}  #=> [14,9]
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-minmax

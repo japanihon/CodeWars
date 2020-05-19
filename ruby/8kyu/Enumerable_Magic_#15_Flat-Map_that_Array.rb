b250ec46b348cc4b8ee5c422d36e196e8e79e61f
sarah <sarahkharraz@gmail.com>
2020-05-19 15:21:18 +0100

    RB - Enumerable Magic #15 - Flat-Map that Array! - ex

---
+++ ruby/8kyu/Enumerable_Magic_#15_Flat-Map_that_Array.rb
@@ -0,0 +1,5 @@
+# Create a method flat_map that accepts a list and a block, runs the block for each item in the list. It should return a new array with the block return values, and that array should be flattened (1-dimensional).
+#
+# If you need help, here's a reference (flat_map and collect_concat are the same):
+#
+# http://www.rubycuts.com/enum-collect-concat

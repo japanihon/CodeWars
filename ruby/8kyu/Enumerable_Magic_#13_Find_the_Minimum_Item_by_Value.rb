e2584957eb8aa56ac6efaa31c806444c8bcba011
sarah <sarahkharraz@gmail.com>
2020-05-19 15:24:29 +0100

    RB - Enumerable Magic #13 - Find the Minimum Item by Value! - ex

---
+++ ruby/8kyu/Enumerable_Magic_#13_Find_the_Minimum_Item_by_Value.rb
@@ -0,0 +1,5 @@
+# Create a method min_by that accepts a list and a block, runs the block for each item in the list, and returns the item with the lowest block return value.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-min-by

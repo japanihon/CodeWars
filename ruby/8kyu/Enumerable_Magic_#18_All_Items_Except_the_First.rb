f18d72fa0b858e54a4ab285192f0cb5758d39b5a
sarah <sarahkharraz@gmail.com>
2020-05-19 15:22:53 +0100

    RB - Enumerable Magic #18 - All Items Except the First... - ex

---
+++ ruby/8kyu/Enumerable_Magic_#18_All_Items_Except_the_First.rb
@@ -0,0 +1,5 @@
+# Create a method drop that accepts a list, and a number n, and returns a copy of the list that skips the first n elements.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-drop

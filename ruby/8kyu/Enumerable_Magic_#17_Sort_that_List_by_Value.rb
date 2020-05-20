6bb3f701690457e8ddc4f003394b7cfa46ae4215
sarah <sarahkharraz@gmail.com>
2020-05-20 15:22:41 +0100

    RB - Enumerable Magic #17 - Sort that List by Value! - ex

---
+++ ruby/8kyu/Enumerable_Magic_#17_Sort_that_List_by_Value.rb
@@ -0,0 +1,5 @@
+# Create a method sort_by that accepts a list and a block, and returns a new list sorted by the return values of the block.
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-sort-by

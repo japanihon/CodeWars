d20a02ac4c4e82534d9e71361b7d11ce10656e18
sarah <sarahkharraz@gmail.com>
2020-05-20 15:26:19 +0100

    RB - Name Shuffler - solution

--- ruby/8kyu/Name_Shuffler.rb
+++ ruby/8kyu/Name_Shuffler.rb
@@ -1,3 +1,9 @@
 # Write a function that returns a string in which firstname is swapped with last name.
 #
 # nameShuffler('john McClane'); => "McClane john"
+
+# SOLUTION
+
+def name_shuffler(word)
+  word.split(" ").reverse.join(" ")
+end

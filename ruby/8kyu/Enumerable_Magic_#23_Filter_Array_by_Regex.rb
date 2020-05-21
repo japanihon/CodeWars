c446554b71f68d438db319d245434a808545fa09
sarah <sarahkharraz@gmail.com>
2020-05-21 12:29:03 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#23_Filter_Array_by_Regex.rb
+++ ruby/8kyu/Enumerable_Magic_#23_Filter_Array_by_Regex.rb
@@ -1,8 +1,13 @@
-# Create a method grep that accepts a list and a regexp pattern, and returns a list of elements which match the pattern.
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-grep
+=begin
+
+INSTRUCTION
+
+Create a method grep that accepts a list and a regexp pattern, and returns a list of elements which match the pattern.
+
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-grep
+
+=end
 
 # SOLUTION
 

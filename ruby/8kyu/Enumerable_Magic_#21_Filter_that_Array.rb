bf05991b9123a0fcc4e1e6792090032bd73b3e25
sarah <sarahkharraz@gmail.com>
2020-05-21 12:27:46 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#21_Filter_that_Array.rb
+++ ruby/8kyu/Enumerable_Magic_#21_Filter_that_Array.rb
@@ -1,8 +1,13 @@
-# Create a method select that accepts a list and a block, and returns a new array of elements for which the given block returns true.
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-select
+=begin
+
+INSTRUCTION
+
+Create a method select that accepts a list and a block, and returns a new array of elements for which the given block returns true.
+
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-select
+
+=end
 
 # SOLUTION
 

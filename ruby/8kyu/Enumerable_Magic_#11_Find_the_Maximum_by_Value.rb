acc856a920fa53daf9f27522ceb6c7994a7c7e9b
sarah <sarahkharraz@gmail.com>
2020-05-21 12:20:36 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#11_Find_the_Maximum_by_Value.rb
+++ ruby/8kyu/Enumerable_Magic_#11_Find_the_Maximum_by_Value.rb
@@ -1,8 +1,13 @@
-# Create a method max_by that accepts a list and a block, runs the block for each item in the list, and returns the item with the highest block return value.
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-max-by
+=begin
+
+INSTRUCTION
+
+Create a method max_by that accepts a list and a block, runs the block for each item in the list, and returns the item with the highest block return value.
+
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-max-by
+
+=end
 
 # SOLUTION
 

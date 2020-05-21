b43fb43c51d1b50a4e5dd54690f437da83da1561
sarah <sarahkharraz@gmail.com>
2020-05-21 12:18:11 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#6_Count_Items.rb
+++ ruby/8kyu/Enumerable_Magic_#6_Count_Items.rb
@@ -1,8 +1,12 @@
-# Create a method count that accepts a list and a block, and returns the total number of items for which the block returns true.
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-count
+=begin
+
+INSTRUCTION
+
+Create a method count that accepts a list and a block, and returns the total number of items for which the block returns true.
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-count
+
+=end
 
 # SOLUTION
 

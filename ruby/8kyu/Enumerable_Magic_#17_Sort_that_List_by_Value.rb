decb71a8c4163afd1e5152475a74a19d51ab821e
sarah <sarahkharraz@gmail.com>
2020-05-21 12:24:37 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#17_Sort_that_List_by_Value.rb
+++ ruby/8kyu/Enumerable_Magic_#17_Sort_that_List_by_Value.rb
@@ -1,8 +1,13 @@
-# Create a method sort_by that accepts a list and a block, and returns a new list sorted by the return values of the block.
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-sort-by
+=begin
+
+INSTRUCTION
+
+Create a method sort_by that accepts a list and a block, and returns a new list sorted by the return values of the block.
+
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-sort-by
+
+=end
 
 # SOLUTION
 

1df24f3f062a7dfd1a4ef98bcb72a50d64275be7
sarah <sarahkharraz@gmail.com>
2020-05-21 12:28:24 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#22_First_Elements_of_an_Array.rb
+++ ruby/8kyu/Enumerable_Magic_#22_First_Elements_of_an_Array.rb
@@ -1,8 +1,13 @@
-# Create a method first that accepts a list and an optional parameter n. If n is unspecified, it returns just the first element of the list. If n is specified, it returns up to that number of elements from the beginning of the list.
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-first
+=begin
+
+INSTRUCTION
+
+Create a method first that accepts a list and an optional parameter n. If n is unspecified, it returns just the first element of the list. If n is specified, it returns up to that number of elements from the beginning of the list.
+
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-first
+
+=end
 
 # SOLUTION
 

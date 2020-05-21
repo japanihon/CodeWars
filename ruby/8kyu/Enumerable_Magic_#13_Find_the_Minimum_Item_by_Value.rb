c368b7aa6fbc667099ed8759085f2608249c8778
sarah <sarahkharraz@gmail.com>
2020-05-21 12:22:36 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#13_Find_the_Minimum_Item_by_Value.rb
+++ ruby/8kyu/Enumerable_Magic_#13_Find_the_Minimum_Item_by_Value.rb
@@ -1,8 +1,13 @@
-# Create a method min_by that accepts a list and a block, runs the block for each item in the list, and returns the item with the lowest block return value.
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-min-by
+=begin
+
+INSTRUCTION
+
+Create a method min_by that accepts a list and a block, runs the block for each item in the list, and returns the item with the lowest block return value.
+
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-min-by
+
+=end
 
 # SOLUTION
 

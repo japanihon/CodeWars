67c9e80788e487b643a0b8f4a48a4f83c3c7b079
sarah <sarahkharraz@gmail.com>
2020-05-21 12:30:20 +0100

    RB - refactoring

--- ruby/8kyu/Enumerable_Magic_#26_Grab_Elements_While.rb
+++ ruby/8kyu/Enumerable_Magic_#26_Grab_Elements_While.rb
@@ -1,13 +1,18 @@
-# Create a method take_while that accepts a list and a block, and returns a array of the first several elements from the list, for as long as the block returns true.
-#
-# Keep in mind, we don't want all the items for which the block returns true; once we get a false response, we want to stop looking. So this example:
-#
-# take_while([0,1,2,3,4,5,6]){|item| item.even?}
-# Will only return [0], not [0,2,4,6].
-#
-# If you need help, here's a reference:
-#
-# http://www.rubycuts.com/enum-take-while
+=begin
+
+INSTRUCTION
+
+Create a method take_while that accepts a list and a block, and returns a array of the first several elements from the list, for as long as the block returns true.
+
+Keep in mind, we don't want all the items for which the block returns true; once we get a false response, we want to stop looking. So this example:
+
+take_while([0,1,2,3,4,5,6]){|item| item.even?}
+Will only return [0], not [0,2,4,6].
+
+If you need help, here's a reference:
+http://www.rubycuts.com/enum-take-while
+
+=end
 
 # SOLUTION
 

997371c0f426a83841a53192f72654e84880e6c3
sarah <sarahkharraz@gmail.com>
2020-05-22 22:58:25 +0100

    RB - refactoring

--- ruby/8kyu/Incorrect_division_method.rb
+++ ruby/8kyu/Incorrect_division_method.rb
@@ -1,4 +1,10 @@
-# This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+=begin
+
+INSTRUCTION
+
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+=end
 
 # SOLUTION
 

f9f3875a88f9b93183b8e91f7c094f4eebf8477b
sarah <sarahkharraz@gmail.com>
2020-05-22 23:06:22 +0100

    RB - refactoring

--- ruby/7kyu/Simple_Fun_#176_Reverse_Letter.rb
+++ ruby/7kyu/Simple_Fun_#176_Reverse_Letter.rb
@@ -1,17 +1,22 @@
-# Task
-# Given a string str, reverse it omitting all non-alphabetic characters.
-#
-# Example
-# For str = "krishan", the output should be "nahsirk".
-#
-# For str = "ultr53o?n", the output should be "nortlu".
-#
-# Input/Output
-# [input] string str
-#
-# A string consists of lowercase latin letters, digits and symbols.
-#
-# [output] a string
+=begin
+
+INSTRUCTION
+
+Task
+Given a string str, reverse it omitting all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+
+=end
 
 # SOLUTION
 

1db151a52110f069b4c29534a39c8c42833a6241
sarah <sarahkharraz@gmail.com>
2020-05-22 23:14:19 +0100

    RB - refactoring

--- ruby/6kyu/CamelCase_Method.rb
+++ ruby/6kyu/CamelCase_Method.rb
@@ -1,9 +1,14 @@
-# Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
-#
-# For instance:
-#
-# "hello case".camelCase() => HelloCase
-# "camel case word".camelCase() => CamelCaseWord
+=begin
+
+INSTRUCTION
+
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+
+=end
 
 # SOLUTION
 

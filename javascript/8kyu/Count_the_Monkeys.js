b1e04920f1740ec076adcf9914911bc6ce42be6d
sarah <sarahkharraz@gmail.com>
2020-05-26 17:11:23 +0100

    JS - refactoring

--- javascript/8kyu/Count_the_Monkeys.js
+++ javascript/8kyu/Count_the_Monkeys.js
@@ -1,18 +1,24 @@
-// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
-//
-// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
-//
-// For example:
-//
-// monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// monkeyCount(1) // --> [1]
+/*
+
+INSTRUCTION
+
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example:
+
+monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+monkeyCount(1) // --> [1]
+
+*/
 
 // SOLUTION
 
 function monkeyCount(n) {
- let emptyArray = []
- for (let i = 1; i <= n; i++){
-   emptyArray.push(i);
-    }
-    return emptyArray
+  let emptyArray = [];
+  for (let i = 1; i <= n; i++) {
+    emptyArray.push(i);
   }
+  return emptyArray;
+}

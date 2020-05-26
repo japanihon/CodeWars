64e0dd64acde5fc10f5f5225ef22780f49ac03d2
sarah <sarahkharraz@gmail.com>
2020-05-26 17:42:36 +0100

    JS - refactoring

--- javascript/8kyu/Reversed_Words.js
+++ javascript/8kyu/Reversed_Words.js
@@ -1,14 +1,20 @@
-// Complete the solution so that it reverses all of the words within the string passed in.
+/*
 
-// Example:
-// reverseWords("The greatest victory is that which requires no battle")
-// should return "battle no requires which that is victory greatest The"
+INSTRUCTION
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+reverseWords("The greatest victory is that which requires no battle")
+should return "battle no requires which that is victory greatest The"
+
+*/
 
 // SOLUTION
 
-function reverseWords(str){
-  let split = str.split(" ");
-  let reverse = split.reverse();
-  let join = reverse.join(" ")
+const reverseWords = str => {
+  const split = str.split(" ");
+  const reverse = split.reverse();
+  const join = reverse.join(" ");
   return join;
-}
+};

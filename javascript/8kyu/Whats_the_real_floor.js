ff35902d438a424d006f2a8846f45001d0825e9e
sarah <sarahkharraz@gmail.com>
2020-05-26 17:49:16 +0100

    JS - refactoring

--- javascript/8kyu/Whats_the_real_floor.js
+++ javascript/8kyu/Whats_the_real_floor.js
@@ -1,24 +1,30 @@
-// Task Overview
-// Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor ('cause of superstition).
-//
-// Write a function that given an American floor (passed as an integer) returns the real floor.
-// Moreover, your function should work for basement floors too: just return the same value as the passed one.
-//
-// Usage Examples
-// getRealFloor(1) == 0
-// getRealFloor(0) == 0 // Special case to please Europeans
-// getRealFloor(5) == 4
-// getRealFloor(15) == 13
-// getRealFloor(-3) == -3
+/*
+
+INSTRUCTION
+
+Task Overview
+Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor ('cause of superstition).
+
+Write a function that given an American floor (passed as an integer) returns the real floor.
+Moreover, your function should work for basement floors too: just return the same value as the passed one.
+
+Usage Examples:
+getRealFloor(1) == 0
+getRealFloor(0) == 0 // Special case to please Europeans
+getRealFloor(5) == 4
+getRealFloor(15) == 13
+getRealFloor(-3) == -3
+
+*/
 
 // SOLUTION
 
-function getRealFloor(n) {
+const getRealFloor = n => {
   if (n <= 0) {
-    return n
-    } else if (n >=1 && n <= 12) {
-      return (n - 1)
-    } else {
-      return (n - 2)
-    }
-}
+    return n;
+  } else if (n >= 1 && n <= 12) {
+    return n - 1;
+  } else {
+    return n - 2;
+  }
+};

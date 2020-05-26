3b588e35806fa0b461c18cf138674755408d82ec
sarah <sarahkharraz@gmail.com>
2020-05-26 17:47:48 +0100

    JS - refactoring

--- javascript/8kyu/Sum_of_positive.js
+++ javascript/8kyu/Sum_of_positive.js
@@ -1,11 +1,19 @@
-// You get an array of numbers, return the sum of all of the positives ones.
-//
-// Example [1,-4,7,12] => 1 + 7 + 12 = 20
-//
-// Note: if there is nothing to sum, the sum is default to 0.
+/*
+
+INSTRUCTION
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example:
+[1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note:
+If there is nothing to sum, the sum is default to 0.
+
+*/
 
 // SOLUTION
 
-function positiveSum(arr) {
+const positiveSum = arr => {
   return arr.reduce((a, b) => (Math.sign(b) === 1 ? a + b : a), 0);
-}
+};

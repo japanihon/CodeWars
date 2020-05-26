7040725855f47da9efdffed725f221d6858db5c2
sarah <sarahkharraz@gmail.com>
2020-05-26 17:46:46 +0100

    JS - refactoring

--- javascript/8kyu/Sum_Mixed_Array.js
+++ javascript/8kyu/Sum_Mixed_Array.js
@@ -1,10 +1,16 @@
-// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
-// Return your answer as a number.
+/*
+
+INSTRUCTION
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+Return your answer as a number.
+
+*/
 
 // SOLUTION
 
-function sumMix(x){
-  var convertString = x.map(Number)
-  var reduceToSum = convertString.reduce((a, b) => a + b, 0)
-return reduceToSum
-}
+const sumMix = x => {
+  const convertString = x.map(Number);
+  const reduceToSum = convertString.reduce((a, b) => a + b, 0);
+  return reduceToSum;
+};

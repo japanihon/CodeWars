08ac561571537fa71ff9d6960f6144905d256d54
sarah <sarahkharraz@gmail.com>
2020-05-26 17:07:52 +0100

    JS - refactoring

--- javascript/8kyu/Calculate_average.js
+++ javascript/8kyu/Calculate_average.js
@@ -1,10 +1,16 @@
-// Write function avg which calculates average of numbers in given list.
+/*
+
+INSTRUCTION
+
+Write function avg which calculates average of numbers in given list.
+
+*/
 
 // SOLUTION
 
 function find_average(array) {
-  var sum = 0;
-  for (var i = 0; i < array.length; i++) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
     sum = sum + array[i];
   }
   return sum / array.length;

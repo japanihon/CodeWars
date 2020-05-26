c8309db457546a4e0dd0a965138f15f9fe04f2dc
sarah <sarahkharraz@gmail.com>
2020-05-26 17:48:38 +0100

    JS - refactoring

--- javascript/8kyu/Sum_without_highest_and_lowest_number.js
+++ javascript/8kyu/Sum_without_highest_and_lowest_number.js
@@ -1,21 +1,27 @@
-// Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
-// (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+/*
 
-// Example:
-// { 6, 2, 1, 8, 10 } => 16
-// { 1, 1, 11, 2, 3 } => 6
+INSTRUCTION
 
-// If array is empty, null or None, or if only 1 Element exists, return 0.
-// Note:In C++ instead null an empty vector is used. In C there is no null. ;-)
+Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+(The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
 
-// -- There's no null in Haskell, therefore Maybe [Int] is used. Nothing represents null.
-// Have fun coding it and please don't forget to vote and rank this kata! :-)
+Example:
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
 
-// I have created other katas. Have a look if you like coding and challenges.
+If array is empty, null or None, or if only 1 Element exists, return 0.
+Note: In C++ instead null an empty vector is used. In C there is no null. ;-)
+
+-- There's no null in Haskell, therefore Maybe [Int] is used. Nothing represents null.
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have created other katas. Have a look if you like coding and challenges.
+
+*/
 
 // SOLUTION
 
-function sumArray(array) {
+const sumArray = array => {
   if (!Array.isArray(array)) return 0;
 
   const sort = array.sort((a, b) => a - b);
@@ -23,4 +29,4 @@ function sumArray(array) {
   const reduce = slice.reduce((a, b) => a + b, 0);
 
   return reduce;
-}
+};

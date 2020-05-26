1a75f5b2c02563afa9ad03b0575037b80e0eeabb
sarah <sarahkharraz@gmail.com>
2020-05-26 17:36:35 +0100

    JS - refactoring

--- javascript/8kyu/noobCode_01_SUPERSIZE_ME_or_rather_this_integer.js
+++ javascript/8kyu/noobCode_01_SUPERSIZE_ME_or_rather_this_integer.js
@@ -1,13 +1,20 @@
-// Write a function that rearranges an integer into its largest possible value.
-//
-// superSize(123456) //654321
-// superSize(105) // 510
-// superSize(12) // 21
-// If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+/*
+
+INSTRUCTION
+
+Write a function that rearranges an integer into its largest possible value.
+
+superSize(123456) //654321
+superSize(105) // 510
+superSize(12) // 21
+
+If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+
+*/
 
 // SOLUTION
 
-function superSize(num) {
+const superSize = num => {
   return parseInt(
     num
       .toString()
@@ -15,4 +22,4 @@ function superSize(num) {
       .sort((a, b) => b - a)
       .join("")
   );
-}
+};

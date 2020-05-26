9630ee8726117bc42129019fca0d87d0f8c7a309
sarah <sarahkharraz@gmail.com>
2020-05-26 17:09:57 +0100

    JS - refactoring

--- javascript/8kyu/Convert_boolean_values_to_strings_Yes_or_No.js
+++ javascript/8kyu/Convert_boolean_values_to_strings_Yes_or_No.js
@@ -1,4 +1,10 @@
-// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+/*
+
+INSTRUCTION
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
 
 // SOLUTION
 

8a56186d55da57419c09014ec3be713f8ec2502e
sarah <sarahkharraz@gmail.com>
2020-05-26 17:08:22 +0100

    JS - refactoring

--- javascript/8kyu/Convert_a_Number_to_a_String.js
+++ javascript/8kyu/Convert_a_Number_to_a_String.js
@@ -1,10 +1,16 @@
-// We need a function that can transform a number into a string.
-//
-// What ways of achieving this do you know?
-//
-// Examples:
-// numberToString(123); // returns '123';`
-// numberToString(999); // returns '999';`
+/*
+
+INSTRUCTION
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+numberToString(123); // returns '123';`
+numberToString(999); // returns '999';`
+
+*/
 
 // SOLUTION
 

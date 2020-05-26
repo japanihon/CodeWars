1b7094a143f7f913e3194aace65b408a69ec2799
sarah <sarahkharraz@gmail.com>
2020-05-26 17:10:23 +0100

    JS - refactoring

--- javascript/8kyu/Convert_number_to_reversed_array_of_digits.js
+++ javascript/8kyu/Convert_number_to_reversed_array_of_digits.js
@@ -1,12 +1,18 @@
-// Convert number to reversed array of digits
-// Given a random number:
-//
-// C#: long;
-// C++: unsigned long;
-// You have to return the digits of this number within an array in reverse order.
-//
-// Example:
-// 348597 => [7,9,5,8,4,3]
+/*
+
+INSTRUCTION
+
+Convert number to reversed array of digits
+Given a random number:
+
+C#: long;
+C++: unsigned long;
+You have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+
+*/
 
 // SOLUTION
 

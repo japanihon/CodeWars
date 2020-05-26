22c8f2acd0df01e72de485a0cf300f216b77be26
sarah <sarahkharraz@gmail.com>
2020-05-26 17:44:51 +0100

    JS - refactoring

--- javascript/8kyu/Square(n)_Sum.js
+++ javascript/8kyu/Square(n)_Sum.js
@@ -1,12 +1,18 @@
-// Complete the square sum function so that it squares each number passed into it and then sums the results together.
-//
-// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+/*
 
+INSTRUCTION
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+Example:
+[1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+*/
 // SOLUTION
 
-function squareSum(numbers) {
+const squareSum = numbers => {
   const mapping = numbers.map(x => x * x);
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
   const reduc = mapping.reduce(reducer, 0);
   return reduc;
-}
+};

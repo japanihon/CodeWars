5c60d64bbf0da0830b7f2b3ede31cd414317d75b
sarah <sarahkharraz@gmail.com>
2020-05-26 17:12:39 +0100

    JS - refactoring

--- javascript/8kyu/Even_or_Odd.js
+++ javascript/8kyu/Even_or_Odd.js
@@ -1,4 +1,10 @@
-// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+/*
+
+INSTRUCTION
+
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
 
 // SOLUTION
 

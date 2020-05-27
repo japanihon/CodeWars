c4d52c85df2c1708899bc64d73c19d28e9db8c72
sarah <sarahkharraz@gmail.com>
2020-05-27 12:23:09 +0100

    JS - refactoring

--- javascript/7kyu/Highest_and_Lowest.js
+++ javascript/7kyu/Highest_and_Lowest.js
@@ -1,23 +1,28 @@
-// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
-//
-// Example:
-//
-// highAndLow("1 2 3 4 5");  // return "5 1"
-// highAndLow("1 2 -3 4 5"); // return "5 -3"
-// highAndLow("1 9 3 4 -5"); // return "9 -5"
-// Notes:
-//
-// All numbers are valid Int32, no need to validate them.
-// There will always be at least one number in the input string.
-// Output string must be two numbers separated by a single space, and highest number is first.
+/*
+
+INSTRUCTION
+
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Example:
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes:
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+
+*/
 
 // SOLUTION
 
-function highAndLow(numbers) {
-  let spl = numbers.split(" ");
-  let map = spl.map(x => Number(x));
-  let max = String(Math.max(...map));
-  let min = String(Math.min(...map));
-  let concat = max.concat(" ", `${min}`);
+const highAndLow = numbers => {
+  const spl = numbers.split(" ");
+  const map = spl.map(x => Number(x));
+  const max = String(Math.max(...map));
+  const min = String(Math.min(...map));
+  const concat = max.concat(" ", `${min}`);
   return concat;
-}
+};

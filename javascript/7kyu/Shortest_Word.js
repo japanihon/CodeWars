2fb859a6d08ce47bfb9521f24bca6f4aad8d933f
sarah <sarahkharraz@gmail.com>
2020-05-27 12:28:36 +0100

    JS - refactoring

--- javascript/7kyu/Shortest_Word.js
+++ javascript/7kyu/Shortest_Word.js
@@ -1,12 +1,18 @@
-// Simple, given a string of words, return the length of the shortest word(s).
-//
-// String will never be empty and you do not need to account for different data types.
+/*
+
+INSTRUCTION
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+*/
 
 // SOLUTION
 
-function findShort(s) {
+const findShort = s => {
   const spl = s.split(" ");
   const shorter = (left, right) => (left.length <= right.length ? left : right);
   const num = spl.reduce(shorter);
   return num.length;
-}
+};

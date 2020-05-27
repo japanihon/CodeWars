911560f552d20db7def99238da0c5f92eccd451e
sarah <sarahkharraz@gmail.com>
2020-05-27 12:27:09 +0100

    JS - refactoring

--- javascript/7kyu/Remove_duplicate_words.js
+++ javascript/7kyu/Remove_duplicate_words.js
@@ -1,20 +1,24 @@
-// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
-//
-// Example:
-//
-// Input:
-//
-// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
-//
-// Output:
-//
-// 'alpha beta gamma delta'
+/*
+
+INSTRUCTION
+
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example
+
+Input:
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+'alpha beta gamma delta'
+
+*/
 
 // SOLUTION
 
-function removeDuplicateWords (s) {
-    const split = s.split(' ')
-    const removeSame = ([...new Set(split)])
-    const join = removeSame.join(' ')
-  return join
-}
+const removeDuplicateWords = s => {
+  const split = s.split(" ");
+  const removeSame = [...new Set(split)];
+  const join = removeSame.join(" ");
+  return join;
+};

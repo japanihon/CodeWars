6da60e2faf4869ee095b850614bbdad33d01a762
sarah <sarahkharraz@gmail.com>
2020-05-27 13:05:03 +0100

    JS - refactoring

--- javascript/6kyu/Split_Strings.js
+++ javascript/6kyu/Split_Strings.js
@@ -1,16 +1,21 @@
-// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
-//
-// Examples:
-//
-// solution('abc') // should return ['ab', 'c_']
-// solution('abcdef') // should return ['ab', 'cd', 'ef']
+/*
+
+INSTRUCTION
+
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+solution('abc') // should return ['ab', 'c_']
+solution('abcdef') // should return ['ab', 'cd', 'ef']
+
+*/
 
 // SOLUTION
 
-function solution(str) {
-  let regex = /([A-Z][A-Z])/gi;
-  let spl = str.split(regex);
-  let map = spl.map(x => (x.length % 2 == 0 ? x : `${x}_`));
-  let filter = map.filter(Boolean);
+const solution = str => {
+  const regex = /([A-Z][A-Z])/gi;
+  const spl = str.split(regex);
+  const map = spl.map(x => (x.length % 2 == 0 ? x : `${x}_`));
+  const filter = map.filter(Boolean);
   return filter;
-}
+};

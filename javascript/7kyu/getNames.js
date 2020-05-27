407cf12cc47917f57ce176df27aad90ddcd3e451
sarah <sarahkharraz@gmail.com>
2020-05-27 12:20:51 +0100

    JS - refactoring

--- javascript/7kyu/getNames.js
+++ javascript/7kyu/getNames.js
@@ -1,18 +1,23 @@
-// The following code is not giving the expected results. Can you figure out what the issue is?
-//
-// The following is an example of data that would be passed in to the function.
-//
-// var data = [
-//   {name: 'Joe', age: 20},
-//   {name: 'Bill', age: 30},
-//   {name: 'Kate', age: 23}
-// ]
-//
-// getNames(data) // should return ['Joe', 'Bill', 'Kate']
+/*
+
+INSTRUCTION
+
+The following code is not giving the expected results. Can you figure out what the issue is?
+The following is an example of data that would be passed in to the function.
+
+var data = [
+  {name: 'Joe', age: 20},
+  {name: 'Bill', age: 30},
+  {name: 'Kate', age: 23}
+]
+
+getNames(data) // should return ['Joe', 'Bill', 'Kate']
+
+*/
 
 // SOLUTION
 
-function getNames(data){
+const getNames = data => {
   const map = data.map(item => item.name);
-return map
-}
+  return map;
+};

682c851d0074051f42611b2995498c2070164972
sarah <sarahkharraz@gmail.com>
2020-05-27 12:45:39 +0100

    JS - refactoring

--- javascript/6kyu/Duplicate_Encoder.js
+++ javascript/6kyu/Duplicate_Encoder.js
@@ -1,22 +1,28 @@
-// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
-//
-// Examples
-// "din"      =>  "((("
-// "recede"   =>  "()()()"
-// "Success"  =>  ")())())"
-// "(( @"     =>  "))(("
-// Notes
-//
-// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+/*
+
+INSTRUCTION
+
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples:
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
+
+Notes:
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+*/
 
 // SOLUTION
 
-function duplicateEncode(word) {
-  let lowerCase = word.toLowerCase();
-  let spl = lowerCase.split("");
-  let map = spl.map((char, _, array) =>
+const duplicateEncode = word => {
+  const lowerCase = word.toLowerCase();
+  const spl = lowerCase.split("");
+  const map = spl.map((char, _, array) =>
     array.indexOf(char) === array.lastIndexOf(char) ? "(" : ")"
   );
-  let join = map.join("");
+  const join = map.join("");
   return join;
-}
+};

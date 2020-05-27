5a271a9d73c6658d5f6f861dfedaee97b4fec76a
sarah <sarahkharraz@gmail.com>
2020-05-27 12:27:48 +0100

    JS - refactoring

--- javascript/7kyu/Reverse_words.js
+++ javascript/7kyu/Reverse_words.js
@@ -1,15 +1,21 @@
-// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
-//
-// Examples
-// "This is an example!" ==> "sihT si na !elpmaxe"
-// "double  spaces"      ==> "elbuod  secaps"
+/*
+
+INSTRUCTION
+
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples:
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+
+*/
 
 // SOLUTION
 
-function reverseWords(str) {
+const reverseWords = str => {
   const spl = str.split(" ");
   const map = spl.map(x => x.split(""));
   const mapping = map.map(x => x.reverse().join(""));
   const join = mapping.join(" ");
   return join;
-}
+};

733c798797518dd8009d0b0d4d97556d9ccb3ca7
sarah <sarahkharraz@gmail.com>
2020-05-27 12:38:24 +0100

    JS - refactoring

--- javascript/6kyu/CamelCase_Method.js
+++ javascript/6kyu/CamelCase_Method.js
@@ -1,21 +1,25 @@
-// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
-//
-// For instance:
-//
-// "hello case".camelCase() => HelloCase
-// "camel case word".camelCase() => CamelCaseWord
+/*
+
+INSTRUCTION
+
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+
+*/
 
 // SOLUTION
 
 String.prototype.camelCase = function() {
-  const capitalise = function(string) {
-    let stringAsArray = string.split("");
+  const capitalise = string => {
+    const stringAsArray = string.split("");
     stringAsArray[0] = stringAsArray[0].toUpperCase();
-
     return stringAsArray.join("");
   };
 
-  const removeBlanks = function(string) {
+  const removeBlanks = string => {
     return string.length > 0;
   };
 

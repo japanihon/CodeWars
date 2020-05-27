2b7804f149c4cfaaa44a8f2d5980c4acd9e5a458
sarah <sarahkharraz@gmail.com>
2020-05-27 12:41:34 +0100

    JS - refactoring

--- javascript/6kyu/Counting_Duplicates.js
+++ javascript/6kyu/Counting_Duplicates.js
@@ -1,23 +1,29 @@
-// Count the number of Duplicates
-// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
-//
-// Example
-// "abcde" -> 0 # no characters repeats more than once
-// "aabbcde" -> 2 # 'a' and 'b'
-// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
-// "indivisibility" -> 1 # 'i' occurs six times
-// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
-// "aA11" -> 2 # 'a' and '1'
-// "ABBA" -> 2 # 'A' and 'B' each occur twice
+/*
+
+INSTRUCTION
+
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example:
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+*/
 
 // SOLUTION
 
-function duplicateCount(text) {
-  let cap = text.toUpperCase();
-  let spl = cap.split("");
-  let sort = spl.sort();
-  let join = sort.join("");
-  let match = join.match(/([^])\1+/g) || [];
-  let length = match.length;
+const duplicateCount = text => {
+  const cap = text.toUpperCase();
+  const spl = cap.split("");
+  const sort = spl.sort();
+  const join = sort.join("");
+  const match = join.match(/([^])\1+/g) || [];
+  const length = match.length;
   return length;
-}
+};

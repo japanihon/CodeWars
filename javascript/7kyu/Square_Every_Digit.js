e44023e1ff6440f57aefd59cb985f26c9e2a45ac
sarah <sarahkharraz@gmail.com>
2020-05-27 12:29:17 +0100

    JS - refactoring

--- javascript/7kyu/Square_Every_Digit.js
+++ javascript/7kyu/Square_Every_Digit.js
@@ -1,15 +1,21 @@
-// Welcome. In this kata, you are asked to square every digit of a number.
-//
-// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
-//
-// Note: The function accepts an integer and returns an integer
+/*
+
+INSTRUCTION
+
+Welcome. In this kata, you are asked to square every digit of a number.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+
+*/
 
 // SOLUTION
 
-function squareDigits(num) {
-  let putIntoString = String(num);
-  let spl = putIntoString.split("");
-  let map = spl.map(x => x ** 2);
-  let join = map.join("");
+const squareDigits = num => {
+  const putIntoString = String(num);
+  const spl = putIntoString.split("");
+  const map = spl.map(x => x ** 2);
+  const join = map.join("");
   return Number(join);
-}
+};

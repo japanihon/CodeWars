a3457084c506cf5858d31f98aba04f196ee42cc8
sarah <sarahkharraz@gmail.com>
2020-05-27 13:00:04 +0100

    JS - refactoring

--- javascript/6kyu/Persistent_Bugger.js
+++ javascript/6kyu/Persistent_Bugger.js
@@ -1,18 +1,19 @@
-// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
-//
-// For example:
-//
-//  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
-//                        // and 4 has only one digit
-//
-//  persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
-//                         // 1*2*6 = 12, and finally 1*2 = 2
-//
-//  persistence(4) === 0 // because 4 is already a one-digit number
+/*
+
+INSTRUCTION
+
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4 // and 4 has only one digit
+persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126 // 1*2*6 = 12, and finally 1*2 = 2
+persistence(4) === 0 // because 4 is already a one-digit number
+
+*/
 
 // SOLUTION
 
-function persistence(numberToMultiply) {
+const persistence = numberToMultiply => {
   let numberOfDigits = 0;
   let numberOfOperations;
 
@@ -24,15 +25,12 @@ function persistence(numberToMultiply) {
     if (String(numberToMultiply).length === 1) {
       break;
     }
-
-    let putInString = String(numberToMultiply);
-    let spl = putInString.split("");
-
+    const putInString = String(numberToMultiply);
+    const spl = putInString.split("");
     const reducer = (accumulator, currentValue) => accumulator * currentValue;
 
     numberToMultiply = spl.reduce(reducer);
     numberOfDigits = String(numberToMultiply).length;
   }
-
   return numberOfOperations;
-}
+};

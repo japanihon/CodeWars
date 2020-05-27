3f02fee24e6f7d49e7641d06759430e6f948c900
sarah <sarahkharraz@gmail.com>
2020-05-27 12:21:36 +0100

    JS - refactoring

--- javascript/7kyu/Help_Bob_count_letters_and_digits.js
+++ javascript/7kyu/Help_Bob_count_letters_and_digits.js
@@ -1,14 +1,24 @@
-// Bob is a lazy man.
-// He needs you to create a method that can determine how many letters and digits are in a given string.
+/*
 
-// Example:
-// "hel2!lo" --> 6
-// "wicked .. !" --> 6
-// "!?..A" --> 1
+INSTRUCTION
+
+Bob is a lazy man.
+He needs you to create a method that can determine how many letters and digits are in a given string.
+
+Example:
+"hel2!lo" --> 6
+"wicked .. !" --> 6
+"!?..A" --> 1
+
+*/
 
 // SOLUTION
-function countLettersAndDigits(input) {
-  const deleteSpecialsChar = input.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\ ]/gi, '')
+
+const countLettersAndDigits = input => {
+  const deleteSpecialsChar = input.replace(
+    /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\ ]/gi,
+    ""
+  );
   const charLeft = deleteSpecialsChar.length;
-return charLeft
-}
+  return charLeft;
+};

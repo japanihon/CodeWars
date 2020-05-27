66baf7dd51dfdb0c7f0e2c8561d69548769e1cc3
sarah <sarahkharraz@gmail.com>
2020-05-27 12:52:08 +0100

    JS - refactoring

--- javascript/6kyu/IQ_Testing.js
+++ javascript/6kyu/IQ_Testing.js
@@ -1,24 +1,29 @@
-// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
-//
-// ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
-//
-// ##Examples :
-//
-// iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
-//
-// iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+/*
+
+INSTRUCTION
+
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+Examples :
+iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+
+*/
 
 // SOLUTION
 
-function iqTest(numbers) {
-  let spl = numbers.split(" ");
-  let map = spl.map(x => Number(x));
+const iqTest = numbers => {
+  const spl = numbers.split(" ");
+  const map = spl.map(x => Number(x));
 
-  let even = [];
-  let odd = [];
+  const even = [];
+  const odd = [];
 
   for (let i = 0; i < map.length; i++) {
-    let position = map[i];
+    const position = map[i];
     position % 2 === 0 ? even.push({ position, i }) : odd.push({ position, i });
   }
 
@@ -28,4 +33,4 @@ function iqTest(numbers) {
   if (odd.length === 1) {
     return odd[0].i + 1;
   }
-}
+};

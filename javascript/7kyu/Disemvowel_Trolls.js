e00a1ea0272c114202c5de466dd1cf6f4857d6a2
sarah <sarahkharraz@gmail.com>
2020-05-27 12:16:55 +0100

    JS - refactoring

--- javascript/7kyu/Disemvowel_Trolls.js
+++ javascript/7kyu/Disemvowel_Trolls.js
@@ -1,14 +1,21 @@
-// Trolls are attacking your comment section!
-// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
-// Your task is to write a function that takes a string and return a new string with all vowels removed.
-// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-// Note: for this kata y isn't considered a vowel.
+/*
+
+INSTRUCTION
+
+Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+Note: for this kata y isn't considered a vowel.
+
+*/
 
 // SOLUTION
 
-function disemvowel(str) {
-  let regex = /[aeiou]/gi;
-  let spl = str.split("");
-  let map = spl.map(letter => letter.replace(regex, ""));
+const disemvowel = str => {
+  const regex = /[aeiou]/gi;
+  const spl = str.split("");
+  const map = spl.map(letter => letter.replace(regex, ""));
   return map.join("");
-}
+};

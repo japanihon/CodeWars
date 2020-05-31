0946fd00e52d9f94e1bad1870eccc3c12d30bbf6
sarah <sarahkharraz@gmail.com>
2020-05-31 14:08:42 +0100

    JS - Count of positives / sum of negatives - solution

--- javascript/8kyu/Count_of_positives_sum_of_negatives.js
+++ javascript/8kyu/Count_of_positives_sum_of_negatives.js
@@ -11,3 +11,22 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
 you should return [10, -65]
 
 */
+
+// SOLUTION
+
+const countPositivesSumNegatives = input => {
+  if (!input || input.length === 0) return [];
+
+  let negArr = [];
+  let posArr = [];
+
+  const maping = input.map(num =>
+    num <= 0 ? negArr.push(num) : posArr.push(num)
+  );
+  const negSum = negArr.reduce((a, b) => a + b, 0);
+  const posCount = posArr.length;
+
+  const finalArr = [posCount, negSum];
+
+  return finalArr;
+};

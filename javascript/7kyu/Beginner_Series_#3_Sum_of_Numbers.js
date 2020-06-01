d9184808867293dc97edaaf33ff290b966a19aa1
sarah <sarahkharraz@gmail.com>
2020-06-01 22:56:54 +0100

    JS - Beginner Series #3 Sum of Numbers - solution

--- javascript/7kyu/Beginner_Series_#3_Sum_of_Numbers.js
+++ javascript/7kyu/Beginner_Series_#3_Sum_of_Numbers.js
@@ -1,6 +1,7 @@
 /*
 
 INSTRUCTION
+
 Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
 
 Note: a and b are not ordered!
@@ -14,3 +15,17 @@ GetSum(-1, 0) == -1 // -1 + 0 = -1
 GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 
 */
+
+// SOLUTION
+
+const GetSum = (a, b) => {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+};

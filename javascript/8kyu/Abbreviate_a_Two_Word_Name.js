fc5a02b94d46ddb3800997416e82b7fc934b60b4
sarah <sarahkharraz@gmail.com>
2020-06-01 19:52:01 +0100

    JS - Abbreviate a Two Word Name - solution

--- javascript/8kyu/Abbreviate_a_Two_Word_Name.js
+++ javascript/8kyu/Abbreviate_a_Two_Word_Name.js
@@ -10,3 +10,14 @@ Sam Harris => S.H
 Patrick Feeney => P.F
 
 */
+
+// SOLUTION
+
+const abbrevName = name => {
+  const split = name.split(" ");
+  const map = split.map(x => x[0]);
+  const string = map.toString();
+  const replace = string.replace(",", ".");
+  const capital = replace.toUpperCase();
+  return capital;
+};

8cee05f51cb25da6208a259409ef2d5b59761a45
sarah <sarahkharraz@gmail.com>
2020-06-08 19:50:30 +0100

    JS - Welcome! - solution

--- javascript/8kyu/Welcome.js
+++ javascript/8kyu/Welcome.js
@@ -33,3 +33,27 @@ IP_ADDRESS_NOT_FOUND - ip address not in the database
 IP_ADDRESS_REQUIRED - no ip address was supplied
 
 */
+
+// SOLUTION
+
+const database = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso"
+};
+
+const greet = language => database[language] || database.english;

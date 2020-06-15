f3afa5552d0164036a9830f5ec0141e9d404c416
sarah <sarahkharraz@gmail.com>
2020-06-15 15:39:35 +0100

    JS - Beginner Series #2 Clock - solution

--- javascript/8kyu/Beginner_Series_#2_Clock.js
+++ javascript/8kyu/Beginner_Series_#2_Clock.js
@@ -11,3 +11,12 @@ past(0, 1, 1) == 61000
 Input constraints: 0 <= h <= 23, 0 <= m <= 59, 0 <= s <= 59
 
 */
+
+// SOLUTION
+
+const past = (h, m, s) => {
+  const hour = Math.floor(h * 60 * 60 * 1000);
+  const minute = Math.floor(m * 60 * 1000);
+  const seconde = Math.floor(s * 1000);
+  return hour + minute + seconde;
+};

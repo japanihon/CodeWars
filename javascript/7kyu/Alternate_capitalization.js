c6c7ae88e258f02cca15c01f64a0b5cdf476f967
sarah <sarahkharraz@gmail.com>
2020-06-18 16:18:00 +0100

    JS - Alternate capitalization - solution

--- javascript/7kyu/Alternate_capitalization.js
+++ javascript/7kyu/Alternate_capitalization.js
@@ -15,3 +15,34 @@ Indexed capitalization
 Even-odd disparity
 
 */
+
+// SOLUTION
+
+const capitalize = s => {
+  const split = s.split("");
+  let lowerFirst = [];
+  let capFirst = [];
+
+  for (let index = 0; index < split.length; index++) {
+    if (index % 2) {
+      const cap = split[index].toUpperCase();
+      const push = lowerFirst.push(cap);
+    } else {
+      const same = lowerFirst.push(split[index]);
+    }
+  }
+
+  for (let index = 0; index < split.length; index++) {
+    if (index % 2) {
+      const same = capFirst.push(split[index]);
+    } else {
+      const cap = split[index].toUpperCase();
+      const push = capFirst.push(cap);
+    }
+  }
+
+  const joinCapFirst = capFirst.join("");
+  const joinLowerFirst = lowerFirst.join("");
+
+  return [joinCapFirst, joinLowerFirst];
+};

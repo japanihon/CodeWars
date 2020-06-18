dc63c3739021c230bd3c66a368ab5131080c6fe1
sarah <sarahkharraz@gmail.com>
2020-06-18 21:48:34 +0100

    JS - Array plus array - solution

--- javascript/8kyu/Array_plus_array.js
+++ javascript/8kyu/Array_plus_array.js
@@ -7,3 +7,11 @@ I'm new to coding and now I want to get the sum of two arrays...actually the sum
 P.S. Each array includes only integer numbers. Output is a number too.
 
 */
+
+// SOLUTION
+
+const arrayPlusArray = (arr1, arr2) => {
+  const reduce1 = arr1.reduce((acc, val) => acc + val);
+  const reduce2 = arr2.reduce((acc, val) => acc + val);
+  return reduce1 + reduce2;
+};

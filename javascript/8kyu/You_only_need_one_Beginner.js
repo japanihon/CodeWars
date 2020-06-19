54db067f03ebe0e8a04a816e04050fae94959780
sarah <sarahkharraz@gmail.com>
2020-06-19 13:53:55 +0100

    JS - You only need one - Beginner - solution

--- javascript/8kyu/You_only_need_one_Beginner.js
+++ javascript/8kyu/You_only_need_one_Beginner.js
@@ -9,3 +9,10 @@ Array can contain numbers or strings. X can be either.
 Return true if the array contains the value, false if not.
 
 */
+
+// SOLUTION
+
+const check = (a, x) => {
+  const include = a.includes(x);
+  return include;
+};

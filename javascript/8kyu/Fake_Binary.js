eb9ee7baac73464231b9893606fb036d9f9b3d37
sarah <sarahkharraz@gmail.com>
2020-07-17 21:38:54 +0100

    JS - Fake Binary - solution

--- javascript/8kyu/Fake_Binary.js
+++ javascript/8kyu/Fake_Binary.js
@@ -5,3 +5,12 @@ INSTRUCTION
 Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
 */
+
+// SOLUTION
+
+const fakeBin = x => {
+  const split = x.split('')
+  const map = split.map(num => num < 5? 0 : 1)
+  const join = map.join('')
+  return join
+}

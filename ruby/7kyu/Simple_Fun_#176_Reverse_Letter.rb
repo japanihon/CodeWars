85619a9ee5ddb6aca65ee0a57e23eeb5ff58f874
SHKZ <sarahkharraz@gmail.com>
2020-07-18 13:31:56 +0100

    micro change

--- ruby/7kyu/Simple_Fun_#176_Reverse_Letter.rb
+++ ruby/7kyu/Simple_Fun_#176_Reverse_Letter.rb
@@ -5,11 +5,11 @@ INSTRUCTION
 Task
 Given a string str, reverse it omitting all non-alphabetic characters.
 
-Example
+Example:
 For str = "krishan", the output should be "nahsirk".
 For str = "ultr53o?n", the output should be "nortlu".
 
-Input/Output
+Input/Output:
 [input] string str
 
 A string consists of lowercase latin letters, digits and symbols.

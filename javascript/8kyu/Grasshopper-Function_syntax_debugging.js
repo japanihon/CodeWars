b5d39bfcde7d7c7b75f0580f6785e9e8329e8e0c
SHKZ <sarahkharraz@gmail.com>
2020-08-06 17:44:04 +0200

    JS - Grasshopper - Function syntax debugging - solution

--- javascript/8kyu/Grasshopper-Function_syntax_debugging.js
+++ javascript/8kyu/Grasshopper-Function_syntax_debugging.js
@@ -5,3 +5,9 @@ INSTRUCTION
 A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
 
 */
+
+// SOLUTION
+
+const main = (verb, noun) => {
+  return verb + noun
+}

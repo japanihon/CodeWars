1e9beff3aec8e3613e8f1d233e5a1b2bf2ad5d3b
SHKZ <sarahkharraz@gmail.com>
2021-06-21 16:24:25 +0100

    Calculate BMI

---
+++ javascript/8kyu/Calculate_BMI.js
@@ -0,0 +1,21 @@
+/*
+
+INSTRUCTION
+
+Write function bmi that calculates body mass index (bmi = weight / height2).
+if bmi <= 18.5 return "Underweight"
+if bmi <= 25.0 return "Normal"
+if bmi <= 30.0 return "Overweight"
+if bmi > 30 return "Obese"
+
+*/
+
+// SOLUTION
+
+const bmi = (weight, height) => {
+  const calcul = weight / (height * height)
+  if(calcul <= 18.5) return "Underweight"
+  if( calcul <= 25.0) return "Normal"
+  if(calcul <= 30.0) return "Overweight"
+  if(calcul > 30) return "Obese"
+}
